400dc5440ff9e72afd2caade729445f80ceba445
amrmahmoud <amrmahmoud.am@hotmail.com>
2020-02-07 15:13:14 -0500

    added timer, added color change for slider

--- WaterWorld/ViewController.swift
+++ WaterWorld/ViewController.swift
@@ -12,17 +12,24 @@ import UserNotifications
 
 class ViewController: UIViewController {
 //    let nc = NotificationCenter.default
+    @IBOutlet weak var timerView: UIStackView!
     @IBOutlet weak var Slider: UISlider!
+    @IBOutlet weak var currTimer: UILabel!
     @IBOutlet weak var TimerLabel: UILabel!
     @IBOutlet weak var Status: UILabel!
     let defaults = UserDefaults.standard
     var sliderVal = Float(3)
+    var t: Timer?
     override func viewDidLoad() {
         super.viewDidLoad()
         if defaults.bool(forKey: "ON") == true {
             Status.text="Reminders are currently: ON"
+            timerView.isHidden = false
+            timerOn()
         }else{
             Status.text="Reminders are currently: OFF"
+            timerView.isHidden = true
+            timerOff()
         }
         if defaults.object(forKey: "timeInterval") == nil {
             defaults.set(sliderVal,forKey:"timeInterval")
@@ -33,8 +40,34 @@ class ViewController: UIViewController {
         TimerLabel.text = "Every "+"\(sliderVal)"+" hours."
         Slider.value = (sliderVal-1)/11
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    func timerOn(){
+        
+        guard t == nil else { return }
+        t = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {(_) in
+            
+            if let date = self.defaults.object(forKey: "activatedOn") as? Date{
+                let x = Int(Date().timeIntervalSince(date))
+                let interval = Int(self.defaults.float(forKey: "timeInterval") * 3600)
+                let diff = interval - (x % interval)
+                let hours = (diff / 3600)
+                let minutes = (diff / 60) % 60
+                let seconds = (diff % 60)
+                self.currTimer.text = String(format: "%02d:%02d:%02d",hours,minutes,seconds)
+                
+            }
+        }
+        t?.fire()
+        
+    }
+    
+    func timerOff(){
+        t?.invalidate()
+        t=nil
+    }
+    
     @IBAction func onStartClick(_ sender: UIButton) {
         
         let content: UNMutableNotificationContent = UNMutableNotificationContent()
@@ -56,20 +89,26 @@ class ViewController: UIViewController {
         }
         defaults.set(true, forKey: "ON")
         Status.text="Reminders are currently: ON"
-        
+        timerView.isHidden = false
+        defaults.set(Date(),forKey:"activatedOn")
+        timerOn()
     }
     @IBAction func onStopClick(_ sender: UIButton) {
         UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
             Status.text="Reminders are currently: OFF"
         defaults.set(false, forKey: "ON")
+        timerView.isHidden = true
+        timerOff()
     }
     @IBAction func onSliderChange(_ sender: UISlider) {
         sliderVal = (roundf(Slider.value*11*10)/10) + 1
         TimerLabel.text = "Every "+"\(sliderVal)"+" hours."
+        Slider.minimumTrackTintColor = UIColor.init(red: 21/255, green: 126/255, blue: 251/255, alpha: 1)
     }
     @IBAction func onSetTimer(_ sender: UIButton) {
         print(sliderVal)
         defaults.set(sliderVal, forKey: "timeInterval")
+        Slider.minimumTrackTintColor = UIColor.init(red: 15/255, green: 175/255, blue: 0/255, alpha: 1)
     }
     
 }
